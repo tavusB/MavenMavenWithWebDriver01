98423e26005241a6b9fff0aecf919d3a3f8cefc4
BN <BN@DESKTOP-M9KICUU>
2018-01-24 18:08:57 -0600

    dear team my version ready for your reviews

---
+++ MavenWithWebDriver01/src/com/codistan/qaclass01/Maven/TestWithMaven02.java
@@ -0,0 +1,24 @@
+package com.codistan.qaclass01.Maven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestWithMaven02 {
+
+@Test
+	
+	public void amazonTest() {
+		
+	
+	System.setProperty ("webdriver.chrome.driver","C:\\Users\\BN\\Desktop\\TAVUS CODISTAN\\chromedriver.exe");
+	
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("https://amazon.com/");
+	
+	driver.findElement(By.xpath("//input[@id=\"twotabsearchtextbox\"]")).sendKeys("fidget" + Keys.ENTER);
+}
+}
